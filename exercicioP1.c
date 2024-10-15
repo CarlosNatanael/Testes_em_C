9102ad58e0e6153db1afc0e5835b91f5786ae7d6
Carlos Natanael <141105332+Srleo12@users.noreply.github.com>
2024-10-15 02:18:57 +0000

    Add files via upload

---
+++ exercicioP1.c
@@ -0,0 +1,60 @@
+/*
+//exercicio1 | Imprimir um numero inteiro
+#include <stdio.h>
+
+int main() {
+
+    int num = 0;
+ 
+    printf("Informe um numero: ");
+    scanf("%d", &num);
+ 
+    printf("O numero informado foi: %d\n", num);
+
+    return 0;
+
+}
+*/
+
+/*
+//exercicio2 | Somar três numeros
+#include <stdio.h>
+
+int main() {
+
+    int numA, numB, numC, soma = 0;
+ 
+    printf("Vamos somar");
+    printf("\nDigite o primeiro numero:");
+    scanf("%d", &numA);
+    printf("Digite o segundo numero:");
+    scanf("%d", &numB);
+    printf("Digite o terceiro numero:");
+    scanf("%d", &numC);
+ 
+    soma = numA+numB+numC;
+ 
+    printf("A soma dos numeros é: %d\n",soma);
+ 
+    return 0;
+}
+*/
+
+/*
+//exercicio3 | imprimir um numero e levar ao quadrado
+#include <stdio.h>
+
+int main() {
+
+    int num, quad = 0;
+ 
+    printf("informe o numero: ");
+    scanf("%d", &num);
+ 
+    quad = num * num;
+ 
+    printf("O quadrado de %d é %d\n", num, quad);
+    return 0;
+
+}
+*/
---
+++ exercicioP2.c
@@ -0,0 +1,62 @@
+/*
+#include <stdio.h>
+
+int main() {
+    
+    int idade;
+    
+    printf("Qual é a sua idade: ");
+    scanf("%d", &idade);
+    
+    if (idade < 18){
+        printf("Você é maior de idade.\n");
+    }else if (idade > 18 && idade < 60){
+        printf("Você é adulto\n");
+    }else{
+        printf("Você é idoso.\n");
+    }
+    
+    printf("Sua idade é %d\n", idade);
+
+    return  0;
+}
+*/
+
+/*
+// exercicio1
+#include <stdio.h>
+
+int main() {
+    
+    int numA, numB, numC, quad = 0;
+    
+    printf("Vamos encontar o quadrado da soma dos três valores !!");
+    printf("\nDigite o primeiro numero: ");
+    scanf("%d", &numA);
+    printf("Digite o segundo numero: ");
+    scanf("%d", &numB);
+    printf("Digite o terceiro numero: ");
+    scanf("%d", &numC);
+    
+    quad = (numA*numA) + (numB*numB) + (numC*numC);
+    
+    printf("A soma do quadrado é: %d\n",quad);
+    
+    return 0;
+            
+}
+*/
+
+//exercicio2
+#include <stdio.h>
+
+int main() {
+
+    
+
+
+    return 0;
+}
+
+
+
