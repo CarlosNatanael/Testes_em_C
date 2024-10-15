2a6fef4597a457d9d8fbc453265607fea041521e
Carlos Natanael <141105332+Srleo12@users.noreply.github.com>
2024-10-15 02:34:20 +0000

    Add files via upload

---
+++ Estrutura_decisao.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int idade;
+    
+    printf("Qual a sua idade ?");
+    scanf("%d", &idade);
+    
+    if (idade < 18){
+         printf("Você é menor de idade. \n");
+    }else if (idade > 18 && idade <60){
+        printf("Você é adulto\n");
+    }else{
+        printf("Você é idoso\n");
+    }
+    
+    printf("Sua idade é %d\n", idade);
+    
+    return 0,
+}
