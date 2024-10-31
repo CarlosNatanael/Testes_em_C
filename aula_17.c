59d71598d99b2b1b537685549eb452098f7b3320
Carlos Natanael <141105332+Srleo12@users.noreply.github.com>
2024-10-31 09:21:54 -0300

    Update aula_17.c

--- aula_17.c
+++ aula_17.c
@@ -37,6 +37,7 @@ Utilizado quando você precisa de um loop
 onde não tenha um numero fixo de elementos
 mas que tenha um criterio de parada.
 */
+/*
 #include <stdio.h>
 
 int main()
@@ -53,6 +54,37 @@ int main()
         printf("Digite o numero: ");
         scanf("%d", &numero);
     }
+    
+    printf("A soma é %d", soma);
+    
+    return 0;
+}
+*/
+//------------------------------------------------------------------------------
+
+/*
+Estrutura de repetição
+do while (faça enquanto)
+
+Utilizando quando você precisa de um loop
+onde não tenha um numero fixo de elementos
+mas que tenha um criterio de parada e a condição
+de parada é checada após a primeira execução
+*/
+#include <stdio.h>
+
+int main()
+{
+    int numero, soma = 0;
+    
+    do{
+        
+        printf("Informe um número: ");
+        scanf("%d", &numero);
+        
+        soma = soma + numero;
+    }
+    while(numero != 0);
     
     printf("A soma é %d", soma);
     
