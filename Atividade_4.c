7d26175a95d34616ea41080e9e9c0c77f6f823cb
Carlos Natanael <141105332+Srleo12@users.noreply.github.com>
2025-01-08 15:37:59 -0300

    Create Atividade_4.c

---
+++ Atividade_4.c
@@ -0,0 +1,42 @@
+// Atividade 1 
+
+/*
+Exercicio 1
+Faça um progama que determine e mostre os cincos multiplos de 3, considerando
+numeros maiores que 0.
+
+#include <stdio.h>
+
+int main()
+{
+    int numero = 1;
+    int contador = 0;
+    
+    printf("Apresentando os 5 primeiros múltiplos de 5: \n");
+    while(contador < 5){
+        if(numero % 5 == 0){
+            printf("O número %d é múltiplos de 5. \n", numero);
+            contador = contador + 1;
+        }
+        numero = numero + 1;
+    }
+    return 0;
+}
+*/
+
+/*
+Exercicio 2
+Escreva um progama que declare um inteiro, inicialize-o ,
+incremente-o de 100 a 100, imprimindo seu valor na tela, até
+que seu valor seja 100000.
+*/
+
+#include <stdio.h>
+
+int main(){
+    int numero = 0;
+    
+    
+    
+    return 0;
+}
